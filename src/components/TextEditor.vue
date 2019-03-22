923ca0d041b6058e945ccb3e9f7785b8fcf5c9ef
bentondrew <benton.s.drew@drewantech.com>
2019-03-22 14:00:29 -0500

    Remove default about view. Add app login and account views. Update Home view to point to new TextEditor component. Leaving in old files as reference at this point.

---
+++ src/components/TextEditor.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="text_editor">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextEditor",
+  props: {
+    msg: String
+  }                         
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TextEditor msg="Text Editor" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TextEditor from "@/components/TextEditor.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    TextEditor
   }
 };
 </script>
