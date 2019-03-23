7d6c150cc68be5c939b812e4253a5df01a7a789b
bentondrew <benton.s.drew@drewantech.com>
2019-03-23 17:55:22 -0500

    Try putting nav bar into own vue component.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,31 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <div id="home_link">
-        <router-link to="/" tag="img" src="/mini_logo.png"></router-link>
-      </div>
-      <div id="session_links">
-        <router-link to="/login" v-show="!sessionAlive()">Login</router-link>
-        <router-link to="/account" v-show="sessionAlive()">Account</router-link>
-        <button @click="logout" v-show="sessionAlive()">Logout</button>
-      </div>
-    </div>
+    <NavBar/>
     <router-view />
   </div>
 </template>
 
 <script>
+import NavBar from "./NavBar.vue"
 export default {
-  methods: {
-    logout: () => {
-      this.$store.mutations.session.commit({ type: "stopSession" });
-    }
-  },
-  computed: {
-    sessionAlive: () => {
-      return this.$store.getters.session.sessionAlive;
-    }
-  }
+  components: {NavBar}
 };
 </script>
 
@@ -36,17 +19,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="nav">
+    <div id="home_link">
+      <router-link to="/" tag="img" src="/mini_logo.png"></router-link>
+    </div>
+    <div id="session_links">
+      <router-link to="/login" v-show="!sessionAlive()">Login</router-link>
+      <router-link to="/account" v-show="sessionAlive()">Account</router-link>
+      <button @click="logout" v-show="sessionAlive()">Logout</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout: () => {
+      this.$store.mutations.session.commit({ type: "stopSession" });
+    }
+  },
+  computed: {
+    sessionAlive: () => {
+      return this.$store.getters.session.sessionAlive;
+    }
+  }
+};
+</script>
+
+<style>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
