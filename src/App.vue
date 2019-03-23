d0dd6da6e33599968a8a1c0fb93cdf2e0306dc77
bentondrew <benton.s.drew@drewantech.com>
2019-03-23 15:38:12 -0500

    More linting changes to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link
-        to="/"
-        tag="img"
-        src="/favicon.ico"
-      ></router-link>
+      <router-link to="/" tag="img" src="/favicon.ico"></router-link>
       | <router-link to="/login">Login</router-link> |
       <router-link to="/account">Account</router-link>
     </div>
