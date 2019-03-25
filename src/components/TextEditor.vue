8c92b50da329377a16fd6840c5fb3df76f1e923b
bentondrew <benton.s.drew@drewantech.com>
2019-03-25 14:06:28 -0500

    Linting changes.

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -3,17 +3,17 @@
     <form class="editor">
       <p>
         <label for="fileName">File Name:</label>
-        <input id="fileName" v-model="fileName" placeholder="example.txt">
+        <input id="fileName" v-model="fileName" placeholder="example.txt" />
       </p>
       <p>
         <label for="fileContent">File Content:</label>
         <textarea id="fileContent" v-model="fileContent"></textarea>
       </p>
       <p>
-        <input type="submit" value="Save">
+        <input type="submit" value="Save" />
       </p>
     </form>
-    <hr>
+    <hr />
     <div>
       <h1>{{ fileName }}</h1>
       <h2>{{ fileContent }}</h2>
@@ -24,13 +24,14 @@
 <script>
 export default {
   name: "TextEditor",
-  data: {
-    fileName: null,
-    fileContent: null,
+  data() {
+    return {
+      fileName: null,
+      fileContent: null
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
