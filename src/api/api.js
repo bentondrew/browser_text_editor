d81dcf06d513a469d98e1e8c442601927ad030ff
bentondrew <benton.s.drew@drewantech.com>
2019-03-27 12:35:36 -0500

    More linting changes.

--- src/api/api.js
+++ src/api/api.js
@@ -8,13 +8,13 @@ class API {
     superagent.get("http://localhost/files/" + userName, function(err, res) {
       if (err) throw err;
       returnArray = res.body;
-    }
-    );
+    });
     return returnArray;
   }
   getFile(userName, fileName) {
     var returnObject = [];
-    superagent.get("http://localhost/files/" + userName + "/" + fileName,
+    superagent.get(
+      "http://localhost/files/" + userName + "/" + fileName,
       function(err, res) {
         if (err) throw err;
         returnObject = res.body;
@@ -27,12 +27,10 @@ class API {
     superagent
       .post("http://localhost/files/" + userName + "/" + fileObject.name)
       .send({ content: fileObject.content })
-      .then(
-        function(err, res) {
-          if (err) throw err;
-          returnObject = res.body;
-        }
-      );
+      .then(function(err, res) {
+        if (err) throw err;
+        returnObject = res.body;
+        });
     return returnObject;
   }
   updateUserFile(userName, fileObject) {
@@ -40,13 +38,12 @@ class API {
     superagent
       .put("http://localhost/files/" + userName + "/" + fileObject.name)
       .send({ content: fileObject.content })
-      .then(
-        function(err, res) {
-          if (err) throw err;
-          returnObject = res.body;
-        }
-      );
-    return returnObject;    }
+      .then(function(err, res) {
+        if (err) throw err;
+        returnObject = res.body;
+      });
+    return returnObject;
+  }
 }
 
 const api = new API();
