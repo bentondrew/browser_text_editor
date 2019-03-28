b7036bc341d38f0c2928eb54dda30b22f497f121
bentondrew <benton.s.drew@drewantech.com>
2019-03-28 16:07:16 -0500

    More linting changes.

--- src/api/index.js
+++ src/api/index.js
@@ -29,7 +29,7 @@ export default {
       .send({ content: fileObject.content })
       .then(res => res.body)
       .then(file => {
-        returnObject = file
+        returnObject = file;
       });
     return returnObject;
   },
@@ -41,7 +41,7 @@ export default {
       .send({ content: fileObject.content })
       .then(res => res.body)
       .then(file => {
-        returnObject = file
+        returnObject = file;
       });
     return returnObject;
   }
